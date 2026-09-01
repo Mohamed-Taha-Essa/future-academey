2ebabb6217239f185a18780e1ca8da4ba012431d
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2026-09-01 23:55:45 +0300

    update requirements

--- project/settings.py
+++ project/settings.py
@@ -109,9 +109,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.environ.get('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
         conn_max_age=600,
-        conn_health_checks=True,
     )
 }
 
